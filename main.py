4de7f53a5da7d11179d3769263edd6b94594f60e
DavidKo3 <automorphicnew@gmail.com>
2018-04-29 20:20:09 +0900

    ..

---
+++ main.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+import torch.optim as optim
+import model as model
+from torch.autograd import Variable
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.,5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+def imshow(img):
+    img = img /2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1,2,0)))
+
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+
+# print labels
+
+print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+if torch.cuda.is_available():
+    print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+
+
+net = model.Net().cuda()
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+        inputs = Variable(inputs.cuda())
+        labels = Variable(labels.cuda())
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        # print(loss)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.data.cpu().numpy()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss/ 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+

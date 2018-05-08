304ead3d2cd00841216674369d3453987255cf8e
DavidKo3 <automorphicnew@gmail.com>
2018-05-08 20:31:08 +0900

    add cnn model with batch nomalization

--- main.py
+++ main.py
@@ -52,11 +52,11 @@ if __name__ == '__main__':
 
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=2)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
-    testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False, num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
 
     classes = ('plane', 'car', 'bird', 'cat',
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
@@ -80,27 +80,28 @@ if __name__ == '__main__':
     # net = model.Net().cuda()
 
     # net.weight_init(net.parameters())
-
-    # ConvNet as fixed feature extractor
-    model_conv = torchvision.models.vgg16(pretrained=True)
-
-    fine_tune_model = model.FineTuneModel(model_conv, config.arch, 10, 1).cuda()
-    # print("==============vgg 16 model structure========================/")
-    # print(fine_tune_model.features)
-    print(fine_tune_model)
-
-
-    fine_tune_model.weight_init(fine_tune_model.classifier.parameters())
+    net = model.CNN().cuda()
+    net.weight_init_cnn()
+    # # ConvNet as fixed feature extractor
+    # model_conv = torchvision.models.vgg16(pretrained=True)
+    #
+    # fine_tune_model = model.FineTuneModel(model_conv, config.arch, 10, 1).cuda()
+    # # print("==============vgg 16 model structure========================/")
+    # # print(fine_tune_model.features)
+    # print(fine_tune_model)
+    #
+    #
+    # fine_tune_model.weight_init(fine_tune_model.classifier.parameters())
 
 
     init_lr = config.lr
 
-    params = list(fine_tune_model.classifier.parameters())
+    # params = list(fine_tune_model.classifier.parameters())
     # print(params)
     criterion = nn.CrossEntropyLoss()
     # optimizer = optim.SGD(params, lr=init_lr, momentum=0.9, weight_decay=0.0005, nesterov=True)
-    optimizer = optim.Adam(params, lr=init_lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.0005)
-    # optimizer = optim.Adam(net.parameters(), lr=1e-03, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.0005 )
+    # optimizer = optim.Adam(params, lr=init_lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.0005)
+    optimizer = optim.Adam(net.parameters(), lr=1e-03, betas=(0.9, 0.999), eps=1e-08, weight_decay=0.0005 )
 
     adaptive_lr = 0.0
     best_acc = 9999
@@ -123,7 +124,7 @@ if __name__ == '__main__':
             optimizer.zero_grad()
 
             # forward + backward + optimize
-            outputs = fine_tune_model(inputs)
+            outputs = net(inputs)
             # print(outputs)
             loss = criterion(outputs, labels)
             loss.backward()
@@ -134,9 +135,9 @@ if __name__ == '__main__':
             running_loss += loss.data[0]
             epoch_loss += loss.data[0]
             # epoch_total_loss += running_loss
-            if i % 64 == 63:    # print every 2000 mini-batches
+            if i % 2000 == 1999:    # print every 2000 mini-batches
                 print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss/ 64))
+                      (epoch + 1, i + 1, running_loss/2000))
                 running_loss = 0.0
 
         avg_epoch_loss = epoch_loss/len(trainloader)
@@ -146,7 +147,7 @@ if __name__ == '__main__':
             best_acc = avg_epoch_loss
             is_best = best_acc
             print("epoch : %d, avg_epoch_loss : %.3f , best_Acc = %.3f" % (epoch + 1, avg_epoch_loss, best_acc))
-            save_checkpoint({'epoch': epoch + 1, 'arch': config.arch, 'state_dict': fine_tune_model.state_dict(), 'best_acc': best_acc}, is_best, epoch+1)
+            save_checkpoint({'epoch': epoch + 1, 'arch': config.arch, 'state_dict': net.state_dict(), 'best_acc': best_acc}, is_best, epoch+1)
             elapsed_time = time.time()- start_time
             print("elapsed_time for check best acc :  ", elapsed_time)
     print('Finished Training')
--- model.py
+++ model.py
@@ -28,6 +28,62 @@ class Net(nn.Module):
         # print(x)
         return x
 
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv = nn.Sequential(
+            # 3 x 128 x 128
+            nn.Conv2d(3, 32, 3, 1, 1),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(0.2),
+
+            # 32 x 128 x 128
+            nn.Conv2d(32, 64, 3, 1, 1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2),
+
+            # 64 x 128 x 128
+            nn.MaxPool2d(2, 2),
+
+            # 64 x 64 x 64
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2),
+
+            # 128 x 64 x 64
+            nn.Conv2d(128, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2),
+
+            # 256 x 64 x 64
+            nn.MaxPool2d(2, 2),
+
+            # 256 x 32 x 32
+            nn.Conv2d(256, 10, 3, 1, 1),
+            nn.BatchNorm2d(10),
+            nn.LeakyReLU(0.2))
+
+        # 256 x 32 x 32
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        # 256 x 1 x 1
+        self.classifier = nn.Linear(10, 10)
+
+    def forward(self, x):
+        features = self.conv(x) # [4 x 10 x 56 x 56 ]
+        flatten = self.avg_pool(features).view(features.size(0), -1)
+        output = self.classifier(flatten)
+        return output
+
+
+    def weight_init_cnn(m):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            m.weight.data.normal_(0.0, 0.02)
+        elif classname.find('BatchNorm') != -1:
+            m.weight.data.normal_(1.0, 0.02)
+            m.bias.data.fill_(0)
+
 class GAG_Net(nn.Module):
     def __init__(self):
         super(GAG_Net, self).__init__()
